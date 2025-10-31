3cd53901d614c74b02ea27d2eb8555aaae613375
6aqrec <150935894+6aqrec@users.noreply.github.com>
2025-10-31 08:56:09 +0200

    Update check_py.bat

--- check_py.bat
+++ check_py.bat
@@ -5,10 +5,34 @@ for /f "delims=" %%i in ('where python 2^>nul') do (
     echo %%i | findstr /i "WindowsApps" >nul
     if errorlevel 1 (
         set PYTHON_EXE=%%i
+        goto found_python
     )
 )
 
 echo Real Python not found.
 
-PAUSE
 
+for /f "delims=" %%i in ('where python 2^>nul') do (
+    echo %%i | findstr /i "WindowsApps" >nul
+    if errorlevel 1 (
+        set PYTHON_EXE=%%i
+        goto found_python
+    )
+)
+
+echo Failed to locate Python executable. Exiting.
+exit /b 1
+
+:found_python
+echo Found Python at: %PYTHON_EXE%
+
+%PYTHON_EXE% -m pip --version >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo pip not found.
+    
+) ELSE (
+    echo pip is installed.
+)
+
+echo Python and pip are ready. Continuing script...
+%PYTHON_EXE% -m pip install requests
