07b90bcf73301c63d7e786df007ddeecf5fdd8c7
6aqrec <150935894+6aqrec@users.noreply.github.com>
2025-10-31 08:52:45 +0200

    Update check_py.bat

--- check_py.bat
+++ check_py.bat
@@ -5,10 +5,10 @@ for /f "delims=" %%i in ('where python 2^>nul') do (
     echo %%i | findstr /i "WindowsApps" >nul
     if errorlevel 1 (
         set PYTHON_EXE=%%i
-        goto found_python
     )
 )
 
 echo Real Python not found.
 
 PAUSE
+
